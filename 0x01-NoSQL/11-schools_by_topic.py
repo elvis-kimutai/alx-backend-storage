b0b3dd36c0ff27ce7ee72a0bed522e865922ec77
elvis-kimutai <cheruiyotkim17@gmail.com>
2024-01-23 15:43:58 -0800

    school

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ function that returns the list of school
+ having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school
+    """
+    return mongo_collection.find({"topics": topic})
